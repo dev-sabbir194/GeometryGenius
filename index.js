e8191cc51571b9f2bc45e0e1cda753da74132c9f
unknown <hossainmdsabbir194@gmail.com>
2023-02-18 19:47:04 +0600

    last three cards js

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ document.getElementById("first-card").addEventListener("click", function(){
   displayData(titleName, result);
   })
 
+
 // *----------------------second card details---------------------*//
 // *--------------------------------------------------------------*//   
 
@@ -38,3 +39,90 @@ document.getElementById("first-card").addEventListener("click", function(){
     // console.log(rectangleFirstInput, rectangleSecondInput)
     displayData(titleName, result);
     })
+
+
+    
+
+// *----------------------third card details----------------------*//
+// *--------------------------------------------------------------*//   
+
+
+document.getElementById("third-card").addEventListener("click", function(){
+  const titleName = document.getElementById("third-title").innerText;
+  const parallelogramFirstInput = document.getElementById("input-5").value;
+  const parallelogramSecondInput = document.getElementById("input-6").value;
+  const condition = parseInt(parallelogramFirstInput) * parseInt(parallelogramSecondInput);
+  const result = condition.toFixed(2);
+// ---------------------validation-----------------------//
+  const inputOne = parseFloat(parallelogramFirstInput);
+  const inputTwo = parseFloat(parallelogramSecondInput);  
+  if (isNaN(inputOne) || isNaN(inputTwo) || inputOne <= 0 || inputTwo <= 0) {
+    return alert("Please enter valid positive numbers for both inputs.");
+  }
+
+  displayData(titleName, result);
+  // console.log(parallelogramFirstInput, parallelogramSecondInput)
+  })
+
+
+  // *---------------------fourth card details----------------------*//
+// *--------------------------------------------------------------*//   
+    
+document.getElementById("fourth-card").addEventListener("click", function(){
+  const titleName = document.getElementById("fourth-title").innerText;
+  const rhombusFirstInput = document.getElementById("input-7").value;
+  const rhombusSecondInput = document.getElementById("input-8").value;
+  const firstnumber = (0.5);
+  const condition = firstnumber * parseInt(rhombusFirstInput) * parseInt(rhombusSecondInput);
+  const result = condition.toFixed(2);
+// ---------------------validation--------------------//
+  const inputOne = parseFloat(rhombusFirstInput);
+  const inputTwo = parseFloat(rhombusSecondInput);
+  if (isNaN(inputOne) || isNaN(inputTwo) || inputOne <= 0 || inputTwo <= 0) {
+    return alert("Please enter valid positive numbers for both inputs.");
+  }
+  displayData(titleName, result);
+  })
+
+
+// *----------------------fifth card details----------------------*//
+// *--------------------------------------------------------------*//   
+
+
+  document.getElementById("fifth-card").addEventListener("click", function(){
+    const titleName = document.getElementById("fifth-title").innerText;
+    const pentagonFirstInput = document.getElementById("input-9").value;
+    const pentagonSecondInput = document.getElementById("input-10").value;
+    const firstnumber = (.05);
+    const condition = firstnumber * parseInt(pentagonFirstInput) * parseInt(pentagonSecondInput)
+    const result = condition.toFixed(2);
+// ------------------------validation----------------------//
+    const inputOne = parseFloat(pentagonFirstInput);
+    const inputTwo = parseFloat(pentagonSecondInput);
+    if (isNaN(inputOne) || isNaN(inputTwo) || inputOne <= 0 || inputTwo <= 0) {
+      return alert("Please enter valid positive numbers for both inputs.");
+    }
+    displayData(titleName, result);
+    })
+
+
+// *----------------------sixth card details----------------------*//
+// *--------------------------------------------------------------*//   
+
+ 
+    document.getElementById("sixth-card").addEventListener("click", function(){
+      const titleName = document.getElementById("sixth-title").innerText;
+      const ellipseFirstInput = document.getElementById("input-11").value;
+      const ellipseSecondInput = document.getElementById("input-12").value; 
+      const pi = (3.14);
+      const condition = pi * parseInt(ellipseFirstInput) * parseInt(ellipseSecondInput);
+      const result = condition.toFixed(2);
+// ---------------------------validation-------------------//
+      const inputOne = parseFloat(ellipseFirstInput);
+      const inputTwo = parseFloat(ellipseSecondInput);
+      if (isNaN(inputOne) || isNaN(inputTwo) || inputOne <= 0 || inputTwo <= 0) {
+        return alert("Please enter valid positive numbers for both inputs.");
+      }
+      // console.log(ellipseFirstInput, ellipseSecondInput)
+      displayData(titleName, result);
+      })
